cb682cf4709c2ed92a2db236d6e72646ff45e315
komiyamma <komiyama0629@gmail.com>
2022-02-28 14:46:04 +0900

    hidemaru-net からの移動

---
+++ hm.NET.src/hmNETStaticLib/hmNET/HmOriginalDecodeMap.cs
@@ -0,0 +1,18 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal sealed partial class hmNETDynamicLib
+{
+    internal class HmOriginalDecodeMap
+    {
+
+        public static readonly UInt32[] decode_map = {
+        };
+    }
+
+}
+*/
