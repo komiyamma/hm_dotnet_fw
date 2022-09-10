29ba4edc70b50016ae376250b7a94749f776b75a
komiyamma <komiyama0629@gmail.com>
2022-09-10 15:06:29 +0900

    超将来 hm.NET6 との互換記述。hm.NETとhm.NET6は完全同一ソースであるため。

--- hm.NET.src/hmNET/hidemaruexe_export.cpp
+++ hm.NET.src/hmNET/hidemaruexe_export.cpp
@@ -6,6 +6,34 @@
 #include <windows.h>
 #include "hidemaruexe_export.h"
 
+#if DOTNET_EXPORTS
+using namespace System;
+using namespace System::Collections::Generic;
+
+System::Reflection::Assembly^ CurrentDomain_AssemblyResolve(Object^ sender, ResolveEventArgs^ args) {
+
+	String^ nameall = args->Name;
+	if (nameall->Contains(L"hm.NET") && nameall->Contains(L".Reference")) {
+		HMODULE hHideExe = GetModuleHandle(NULL);
+		wchar_t szHidemaruFullPath[512] = L"";
+		GetModuleFileName(hHideExe, szHidemaruFullPath, _countof(szHidemaruFullPath));
+		String^ hmexepath = gcnew String(szHidemaruFullPath);
+		String^ dir = System::IO::Path::GetDirectoryName(hmexepath);
+		
+		int findcolon_ix = nameall->IndexOf(",");
+		String^name = nameall->Remove(findcolon_ix);
+		String^ fullpath = dir + "\\" + name + ".dll";
+
+		if (System::IO::File::Exists(fullpath)) {
+			return System::Reflection::Assembly::LoadFile(fullpath);
+		}
+	}
+
+	System::Diagnostics::Trace::WriteLine(args->Name + "‚ª—v‹‚³‚ê‚Ü‚µ‚½‚ªA”­Œ©‚Å‚«‚Ü‚¹‚ñ‚Å‚µ‚½B");
+
+	return nullptr;
+}
+#endif
 
 PFNGetDllFuncCalledType Hidemaru_GetDllFuncCalledType = NULL;
 
@@ -16,7 +44,15 @@ struct CHidemaruExeExporter {
 		if (hHideExe) {
 			Hidemaru_GetDllFuncCalledType = (PFNGetDllFuncCalledType)GetProcAddress(hHideExe, "Hidemaru_GetDllFuncCalledType");
 		}
+
+#if DOTNET_EXPORTS
+		System::AppDomain::CurrentDomain->AssemblyResolve += gcnew System::ResolveEventHandler(&CurrentDomain_AssemblyResolve);
+#endif
 	}
 };
+
+
+
+
 
 CHidemaruExeExporter init;
